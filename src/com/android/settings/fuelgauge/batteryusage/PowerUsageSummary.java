fd174790bdb57225aabd70b53b71df979a6a4c18
Trishiraj <gamerprince.exp@gmail.com>
2022-09-07 19:49:00 +0000

    BatteryHealth: Add Battery cycle, temp, capacity and a new design
    Co-authored-by: Lucchetto <lucchetto.tie@live.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Co-authored-by: AshutoshSundresh <62063875+AshutoshSundresh@users>
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    
    fix BatteryHealth Layout

--- src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/batteryusage/PowerUsageSummary.java
@@ -18,6 +18,7 @@ package com.android.settings.fuelgauge.batteryusage;
 
 import static com.android.settings.fuelgauge.BatteryBroadcastReceiver.BatteryUpdateType;
 
+import android.annotation.Nullable;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.database.ContentObserver;
@@ -31,6 +32,8 @@ import android.provider.Settings.Global;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.loader.app.LoaderManager;
@@ -53,6 +56,11 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settingslib.widget.LayoutPreference;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.Integer;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -69,17 +77,19 @@ public class PowerUsageSummary extends PowerUsageBase implements
     static final String KEY_BATTERY_ERROR = "battery_help_message";
     @VisibleForTesting
     static final String KEY_BATTERY_USAGE = "battery_usage_summary";
+    @VisibleForTesting
+    static final String KEY_BATTERY_STAT = "battery_stat";
 
     private static final String KEY_BATTERY_TEMP = "battery_temperature";
 
     static final int MENU_STATS_RESET = Menu.FIRST + 1;
 
     @VisibleForTesting
-    PowerGaugePreference mBatteryTempPref;
-    @VisibleForTesting
     PowerUsageFeatureProvider mPowerFeatureProvider;
     @VisibleForTesting
     BatteryUtils mBatteryUtils;
+    @VisibleForTesting
+    LayoutPreference mBatteryStatPref;
     @VisibleForTesting
     LayoutPreference mBatteryLayoutPref;
     @VisibleForTesting
@@ -95,6 +105,8 @@ public class PowerUsageSummary extends PowerUsageBase implements
     Preference mHelpPreference;
     @VisibleForTesting
     Preference mBatteryUsagePreference;
+    
+    boolean mBatteryHealthSupported;
 
     @VisibleForTesting
     final ContentObserver mSettingsObserver = new ContentObserver(new Handler()) {
@@ -173,7 +185,23 @@ public class PowerUsageSummary extends PowerUsageBase implements
         initFeatureProvider();
         initPreference();
 
-        mBatteryTempPref = (PowerGaugePreference) findPreference(KEY_BATTERY_TEMP);
+        mBatteryStatPref = (LayoutPreference) findPreference(KEY_BATTERY_STAT);
+
+        mBatteryHealthSupported = getResources().getBoolean(R.bool.config_supportBatteryHealth);
+        if (!mBatteryHealthSupported) {
+            getPreferenceScreen().removePreference(mBatteryStatPref);
+        }
+
+        // Set battery stat specifics
+        final TextView mBatteryCapacity = (TextView) mBatteryStatPref.findViewById(R.id.BatteryCapacity);
+        final TextView mBatteryCycle = (TextView) mBatteryStatPref.findViewById(R.id.BatteryCycle);
+        final TextView mBatteryTemp = (TextView) mBatteryStatPref.findViewById(R.id.BatteryTemp);
+
+        mBatteryTemp.setText(BatteryInfo.batteryTemp + " \u2103");
+        mBatteryCapacity.setText(parseBatterymAhText(getResources().getString(R.string.config_batteryCalculatedCapacity))
+        +"/"+ parseBatterymAhText(getResources().getString(R.string.config_batteryDesignCapacity)));
+        mBatteryCycle.setText(parseBatteryCycle(getResources().getString(R.string.config_batteryChargeCycles)));
+
         mBatteryUtils = BatteryUtils.getInstance(getContext());
 
         if (Utils.isBatteryPresent(getContext())) {
@@ -283,12 +311,6 @@ public class PowerUsageSummary extends PowerUsageBase implements
         }
         // reload BatteryInfo and updateUI
         restartBatteryInfoLoader();
-
-        if (BatteryInfo.batteryTemp != 0f) {
-            mBatteryTempPref.setSummary(BatteryInfo.batteryTemp / 10 + " °C");
-        } else {
-            mBatteryTempPref.setSummary(getResources().getString(R.string.status_unavailable));
-        }
     }
 
     @VisibleForTesting
@@ -352,6 +374,49 @@ public class PowerUsageSummary extends PowerUsageBase implements
     @Override
     public void onBatteryTipHandled(BatteryTip batteryTip) {
         restartBatteryTipLoader();
+    }
+
+        private String parseBatterymAhText(String file) {
+        try {
+            return Integer.parseInt(readLine(file)) / 1000 + " mAh";
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot read battery capacity from "
+                    + file, ioe);
+        } catch (NumberFormatException nfe) {
+            Log.e(TAG, "Read a badly formatted battery capacity from "
+                    + file, nfe);
+        }
+        return getResources().getString(R.string.status_unavailable);
+    }
+
+    /**
+    * Reads a line from the specified file.
+    *
+    * @param filename The file to read from.
+    * @return The first line up to 256 characters, or <code>null</code> if file is empty.
+    * @throws IOException If the file couldn't be read.
+    */
+    @Nullable
+    private String readLine(String filename) throws IOException {
+        final BufferedReader reader = new BufferedReader(new FileReader(filename), 256);
+        try {
+            return reader.readLine();
+        } finally {
+            reader.close();
+        }
+    }
+
+    private String parseBatteryCycle(String file) {
+        try {
+            return Integer.parseInt(readLine(file)) + " Cycles";
+        } catch (IOException ioe) {
+            Log.e(TAG, "Cannot read battery cycle from "
+                    + file, ioe);
+        } catch (NumberFormatException nfe) {
+            Log.e(TAG, "Read a badly formatted battery cycle from "
+                    + file, nfe);
+        }
+        return getResources().getString(R.string.status_unavailable);
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
